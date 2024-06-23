c0688e3d521c0e411c9f1f6c123f690ee1d15680
Tiisetso <Tiisetsoatmash@gmail.com>
2024-06-23 21:51:46 +0200

    solved the area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Define the two variables 
+length = 10
+width = 5
+
+# Calculate the area of the rectangle
+Area = length * width 
+
+# Results of the calculation
+print(f"the area of the rectangle is:", Area)
