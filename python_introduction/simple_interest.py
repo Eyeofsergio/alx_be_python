8f3765d37d435466f065295c702ef1bf3bebe0df
Tiisetso <Tiisetsoatmash@gmail.com>
2024-06-23 21:44:00 +0200

    create new simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,12 @@
 # Define variables
-p = principle = 1000 # Principle amount in dollars
-r = rate = 0.05 # Representing the 5% annaul interest rate
-t = time = 3 # Time in years
+principle = 1000 # Principle amount in dollars
+rate = 0.05 # Representing the 5% annaul interest rate
+time = 3 # Time in years
 
 # Calculate simple interest
-i = interest = principle * rate * time 
+interest = principle * rate * time 
 
 
 # Print the calculated intrest
-print("The simple interest is:",  i)
+print(f"The simple interest is:",  interest)
 
