c5221f5840e8570e1289955033a2f2de172d78af
Tiisetso <Tiisetsoatmash@gmail.com>
2024-06-23 20:45:39 +0200

    create simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Define variables
+principle = 1000 # Principle amount in dollars
+rate = 0.05 # Representing the 5% annaul interest rate
+time = 3 # Time in years
+
+# Calculate simple interest
+interest = principle * rate * time 
+interest = 1000 * 0.05 * 3
+
+# Print the calculated intrest
+print("The simple interest is: {interest}")
