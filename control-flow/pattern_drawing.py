52526477bdcffb4d4515cd73cd5adc996abf5926
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-01 21:45:09 +0200

    added the int function

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,11 @@
 #Ask the user to input a positive integer that represents the size of the pattern
-size = float(input("Enter the size of the pattern:") )
+size = int(input("Enter the size of the pattern: "))
 
 if size <= 0:
     print("The size should be a number above 0. ") 
 
 row = 0
-while row < size: 
+for row in range(size): 
     for _ in range(size):
         print("*", end="")
     
