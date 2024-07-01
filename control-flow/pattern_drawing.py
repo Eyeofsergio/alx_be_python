4dbeb9271640e82dd944e27789c89ca7e49035f6
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-01 21:51:29 +0200

    used the for row session

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,11 +2,11 @@
 size = int(input("Enter the size of the pattern: "))
 
 if size <= 0:
-    print("The size should be a number above 0. ") 
+    print(f"The size should be a number above 0. ") 
 
 row = 0
-for row in range(size): 
+for row in range (size): 
     for _ in range(size):
         print("*", end="")
-    
-
+    print()
+    row += 1
