2b1e7ba04bbdbd2bed4ae2eb5527ee9954ad1cf8
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-07 22:52:13 +0200

    shopping  edited

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,4 +1,4 @@
-def display_menu(shopping_list):
+def display_menu():
     print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
@@ -6,7 +6,7 @@ def display_menu(shopping_list):
     print("4. Exit")
 
 def main():
-    shopping_list = {}
+    shopping_list = ()
     while True:
         display_menu()
         choice = input("Enter your choice: ")
