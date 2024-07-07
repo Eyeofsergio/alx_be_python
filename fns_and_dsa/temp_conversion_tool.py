13c712b69c5cb72c35135fbe7677988559683ef7
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-07 22:09:19 +0200

    twmp conversion

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,16 @@
+# Define the global conversion factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+# Implement Conversion Functions
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+# User Interaction
+def main():
+    temperature = float(input("Enter the temperature: "))
+    
+
