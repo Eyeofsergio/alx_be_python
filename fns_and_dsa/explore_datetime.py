7a1fb7dcd166e203f694b0bd3051d2305700aac7
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-07 21:50:22 +0200

    date time update

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,6 +9,6 @@ def calculate_future_date():
     days_to_add = int(input("Enter the number of days to add:"))
     current_date = datetime.now()
     future_date = current_date + timedelta(days=days_to_add)
-    formatted_future_data = future_date
+    formatted_future_data = future_date = future_date("%Y-%m-%d")
     print(f"Future Date (after {days_to_add}days): {"formatted_future_date"}")
 
