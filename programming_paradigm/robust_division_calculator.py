f67ea469306b58f086180012f539ace87f285a97
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-15 21:28:17 +0200

    added robust calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+# Safe divide class
+
+def safe_divide(numerator, denominator):
+    
+    try:
+        num = float(numerator)
+        denom = float(denominator)
+        if denominator == 0:
+            return "Error: Cannot divide by zero."
+        result = numerator/denominator
+        return f"The result of the division ia {result}"
+    except ValueError:
+        return "Error: Please enter numeric vaules only."
