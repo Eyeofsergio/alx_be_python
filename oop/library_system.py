cefc6fc1b5be85e4599715e3f4c3e40e2335f3c1
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-21 02:20:51 +0200

    .append added

--- oop/library_system.py
+++ oop/library_system.py
@@ -28,12 +28,13 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        self.books.append = (book) 
+        self.books.append(book)
+        
 
     def list_books(self):
-
+ 
         for book in self.books:
-            print(book)       
+          print(book)       
 
 
 
